871d1c1868cd10f6136447922c2419455b697091
dvirtz <dvirtz@gmail.com>
2020-10-27 20:27:56 +0200

    match expected output

---
+++ packages/reveal-test/test/compile.test.js
@@ -0,0 +1,53 @@
+import { compile } from 'reveal-test';
+import assert from 'assert';
+
+describe('compile', function() {
+  this.timeout(10000);
+
+  it('succeeds on matching output', async function() {
+    const info = {
+      source: `#include <iostream>
+
+int main() {
+  std::cout << "Hello\\n" << "World\\n";
+}
+`,
+      language: 'c++',
+      compiler: 'g102',
+      options: '-O2 -march=haswell -Wall -Wextra -pedantic -Wno-unused-variable -Wno-unused-parameter',
+      libs: [],
+      execute: true,
+      baseUrl: 'https://godbolt.org/',
+      path: `demo.md:19`,
+      expectedOutput: 'Hello'
+    };
+    await compile(info);
+  });
+
+  it('fails on mismatching output', async function() {
+    const info = {
+      source: `#include <iostream>
+
+int main() {
+  std::cout << "Hello\\n" << "World\\n";
+}
+`,
+      language: 'c++',
+      compiler: 'g102',
+      options: '-O2 -march=haswell -Wall -Wextra -pedantic -Wno-unused-variable -Wno-unused-parameter',
+      libs: [],
+      execute: true,
+      baseUrl: 'https://godbolt.org/',
+      path: `demo:19`,
+      expectedOutput: 'Hello You'
+    };
+    await assert.rejects(compile(info), {
+      name: 'CompileError',
+      message: `demo:19:
+output mismatch:
+actual: Hello
+World
+expected: Hello You`
+    });
+  });
+});
